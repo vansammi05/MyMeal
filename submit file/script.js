ab683d80303474a043d6df8704249ddaa615a8a0
Vansh Sammi <134444161+vansammi05@users.noreply.github.com>
2023-06-10 14:57:15 -0700

    Add files via upload

---
+++ submit file/script.js
@@ -0,0 +1,7 @@
+let a = document.getElementById("btn").addEventListener('click',function(){
+    location.reload();
+    alert('Your form is submitted')
+})
+let b = document.getElementById('btn23').addEventListener('click',function(){
+    location.href = 'file:///C:/Users/Jasvinder%20Pal/OneDrive/Desktop/Food%20Website/index.html'
+})
