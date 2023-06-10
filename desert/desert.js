c1bbacfa3a28f07be8b0a0432a2749b07b488b50
Vansh Sammi <134444161+vansammi05@users.noreply.github.com>
2023-06-10 15:01:33 -0700

    Add files via upload

---
+++ desert/desert.js
@@ -0,0 +1,56 @@
+var noti = document.querySelector('h1');
+	var select = document.querySelector('.select');
+	var button = document.getElementsByTagName('button');
+	for(var but of button){
+		but.addEventListener('click', (e)=>{
+			var add = Number(noti.getAttribute('data-count') || 0);
+			noti.setAttribute('data-count', add +1);
+			noti.classList.add('zero')
+
+			// image --animation to cart ---//
+			// var image = e.target.parentNode.querySelector('img');
+			// var span = e.target.parentNode.querySelector('span');
+			// var s_image = image.cloneNode(false);
+			// span.appendChild(s_image);
+			// span.classList.add("active");
+			// setTimeout(()=>{
+			// 	span.classList.remove("active");
+			// 	span.removeChild(s_image);
+			// }, 500); 
+
+
+			// copy and paste //
+			var parent = e.target.parentNode;
+			var clone = parent.cloneNode(true);
+			select.appendChild(clone);
+			clone.lastElementChild.innerText = "Buy-now";
+
+			if (clone) {
+				noti.onclick = ()=>{
+					select.classList.toggle('display');
+				}	
+			}
+		})
+	}
+let a  = document.querySelector('.btn99').addEventListener('click',function(){
+	location.href = 'file:///C:/Users/Jasvinder%20Pal/OneDrive/Desktop/Food%20Website/index.html'
+})
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
